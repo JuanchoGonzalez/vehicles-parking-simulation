3f6847ae0edc9485a71bf4ce63b2891428843629
Bruno Conti <brunocse.5@gmail.com>
2025-06-19 15:35:52 -0300

    generador de arribos

---
+++ sources/generador_arribos.cpp
@@ -0,0 +1,51 @@
+#include "generador_arribos.h"
+#include "../random/lib/randomc.cpp"
+#include "../random/lib/stocc.cpp"
+void generador_arribos::init(double t,...) {
+//The 'parameters' variable contains the parameters transferred from the editor.
+va_list parameters;
+va_start(parameters,t);
+//To get a parameter: %Name% = va_arg(parameters,%Type%)
+//where:
+//      %Name% is the parameter name
+//	%Type% is the parameter type
+id = 0;
+r = rng.Random();
+while (r <= 0.0 || r >= 1.0) {
+    r = rng.Random();
+}
+sigma = -(1.0/0.025) * log(1 - r);
+}
+double generador_arribos::ta(double t) {
+//This function returns a double.
+return sigma;
+}
+void generador_arribos::dint(double t) {
+id++;
+r = rng.Random();
+while (r <= 0.0 || r >= 1.0) {
+    r = rng.Random();
+}
+sigma = -(1.0/0.025) * log(1 - r);
+}
+void generador_arribos::dext(Event x, double t) {
+//The input event is in the 'x' variable.
+//where:
+//     'x.value' is the value (pointer to void)
+//     'x.port' is the port number
+//     'e' is the time elapsed since last transition
+
+}
+Event generador_arribos::lambda(double t) {
+//This function returns an Event:
+//     Event(%&Value%, %NroPort%)
+//where:
+//     %&Value% points to the variable which contains the value.
+//     %NroPort% is the port number (from 0 to n-1)
+y = id;
+return Event(&y, 0);
+}
+void generador_arribos::exit() {
+//Code executed at the end of the simulation.
+
+}
---
+++ sources/generador_arribos.h
@@ -0,0 +1,33 @@
+//CPP:sources/generador_arribos.cpp
+#if !defined generador_arribos_h
+#define generador_arribos_h
+
+#include "simulator.h"
+#include "event.h"
+#include "stdarg.h"
+
+#include "ctime"
+#include "ctime"
+#include "../random/lib/randomc.h"
+#include "../random/lib/stocc.h"
+
+
+class generador_arribos: public Simulator { 
+// Declare the state,
+// output variables
+// and parameters
+double sigma;
+double id;
+double r;
+CRandomMersenne rng;
+double y;
+public:
+	generador_arribos(const char *n): Simulator(n), rng(static_cast<int>(std::time(NULL))) {};
+	void init(double, ...);
+	double ta(double t);
+	void dint(double);
+	void dext(Event , double );
+	Event lambda(double);
+	void exit();
+};
+#endif
